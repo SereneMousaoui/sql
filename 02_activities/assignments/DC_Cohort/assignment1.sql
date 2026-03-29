9bae605728566ad68c143772f5466a9471d6bd81
Serene Moussaoui <serene.moussaoui@mail.utoronto.ca>
2026-03-29 16:46:29 -0400

    finished sections 1 and 2, waiting for lesson on section 3 on Monday

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -6,7 +6,8 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 --QUERY 1
-
+SELECT * 
+FROM customer;
 
 
 
@@ -16,7 +17,10 @@
 /* 2. Write a query that displays all of the columns and 10 rows from the customer table, 
 sorted by customer_last_name, then customer_first_ name. */
 --QUERY 2
-
+SELECT * 
+FROM customer
+ORDER BY customer_first_name, customer_last_name
+LIMIT 10;
 
 
 
@@ -26,7 +30,10 @@ sorted by customer_last_name, then customer_first_ name. */
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. 
 Limit to 25 rows of output. */
-
+SELECT * 
+FROM customer_purchases
+WHERE product_id = 4 OR  product_id = 10
+LIMIT 25;
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
@@ -36,7 +43,12 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 Limit to 25 rows of output.
 */
 --QUERY 3
-
+SELECT *
+-- make a new column price
+, quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id BETWEEN 8 AND 10
+LIMIT 25;
 
 
 
@@ -49,9 +61,14 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 --QUERY 4
+SELECT product_id, product_name
 
+, CASE
+		WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk'
+  END AS prod_qty_type_condensed 
 
-
+FROM product;
 
 --END QUERY
 
@@ -60,7 +77,19 @@ if the product_qty_type is “unit,” and otherwise displays the word “bulk.�
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 --QUERY 5
+SELECT product_id, product_name
 
+, CASE
+		WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk'
+  END AS prod_qty_type_condensed 
+  
+  , CASE
+		WHEN LOWER(product_name) LIKE '%pepper%' THEN 1
+		ELSE 0
+	END AS  pepper_flag 
+
+FROM product;
 
 
 
@@ -73,6 +102,14 @@ vendor_id field they both have in common, and sorts the result by market_date, t
 Limit to 24 rows of output. */
 --QUERY 6
 
+SELECT *
+
+FROM vendor AS v
+INNER JOIN vendor_booth_assignments AS vba
+			ON v.vendor_id = vba.vendor_id
+
+ORDER BY market_date, vendor_name
+LIMIT 24;
 
 
 
